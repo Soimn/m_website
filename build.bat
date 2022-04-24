c73eb56052561c9f0d14af25f750c25d2f4381f9
Simon Doksrød <simon.doksrod@outlook.com>
2022-04-24 21:27:09 +0200

    devlog 000 update

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ set "diag_options= /nologo /FC /diagnostics:column /W4 /wd4201 /wd4100"
 IF NOT EXIST .\build mkdir build
 pushd .\build
 
-set "compile_options= /DMM_DEBUG %nocrt_options% %diag_options% /Od /Zo /Zf /Z7"
+set "compile_options= %diag_options% /Od /Zo /Zf /Z7"
 set "link_options= /INCREMENTAL:NO /opt:ref"
 
 cl %compile_options% ..\src\main.c /link %link_options% /PDB:builder.pdb /out:builder.exe
